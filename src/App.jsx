5a44efb5c997fa7554564b1c56ed60bcff4af0ea
arturit22 <artur.golubevs@gmail.com>
2025-11-22 11:36:27 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './modules/Header/Header'
+import Navbar from './modules/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ src/modules/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import styles from "./Navbar.module.css"
+
+export default function Navbar() {
+    return (
+        <>
+        <nav className={styles.navbar}>
+            <div className={styles.leftContent}>
+                <a href="">Калькулятор</a>
+                <a href="">О нас</a>
+                <a href="">Гарантия</a>
+                <a href="">Отзывы</a>
+            </div>
+
+            <div className={styles.rightContent}>
+                <button className={styles.button}>Оставить заявку</button>
+            </div>
+        </nav>
+        </>
+    )
+}
